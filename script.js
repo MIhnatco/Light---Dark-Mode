972e68e43c4bbd3152fa1264bedf37446f3a7e0d
Ihliana <Ihnatco@protonmail.com>
2023-12-10 15:16:23 +0100

    update script.js
    create darkMode() and lightMode() to switch modes

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
 const toggleSwitch = document.querySelector("input[type='checkbox']");
+const nav = document.getElementById('nav');
+
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox  = document.getElementById('text-box')
+
+
+//Dark or Light Images 
+function imageMode(color){
+    image1.src = `./images/undraw_projections_re_ulc6_${color}.svg`
+    image2.src = `./images/undraw_real_time_analytics_re_yliv_${color}.svg`
+    image3.src = `./images/undraw_well_done_re_3hpo_${color}.svg`
+}
+
+
+//Dark Mode Styles 
+function darkMode(){
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = "Dark Mode";
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon'); //DRY
+    imageMode('dark')
+
+}
+
+//Light Mode Style 
+function lightMode(){
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = "Light Mode";
+    toggleIcon.children[1].classList.remove('fa-moon');
+    toggleIcon.children[1].classList.add('fa-sun');
+    imageMode('light')
+
+
+}
 
 //Switch Theme dynamically
 function switchTheme(event){
     if(event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
+     
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
     }
 }
 
